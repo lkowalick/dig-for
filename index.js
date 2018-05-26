ba35449002e17cb5fe8e67424a83112c419791f5
Ryan Kowalick <rkowalick@gmail.com>
2018-05-26 00:01:30 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var handler = {
+  apply: (obj, _, args) => obj().path.reduce((accum, pathSegment) => accum[pathSegment], args[0]),
+  get: (obj, prop) => new Proxy(() => ({ path: obj().path.concat([prop]) }), handler),
+};
+
+export var dig = new Proxy(() => ({ path: [] }), handler);
