e640094880aab10eff0f27a30b2981cebd769f32
Ryan Kowalick <rkowalick@gmail.com>
2018-05-29 00:52:30 -0400

    Add specs and not-yet-working o

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ var forHandler = {
 exports.for = new Proxy(() => ({ path: [] }), forHandler);
 
 var oHandler = {
-  apply: (obj, _, args) => obj().path.reduce((accum, pathSegment) => accum[pathSegment], args[0]),
-  get: (obj, prop) => new Proxy(() => ({ path: obj().path.concat([prop]) }), oHandler),
+  apply: (obj, thisArg, args) =>  {
+    console.log("MAH FUNCTIONS: ", obj().functions);
+    console.log("argument: ", args[0]);
+    console.log("thisArg be: ", Object.keys(thisArg));
+    console.log("this be: ", Object.keys(this));
+    obj().functions.reduce((accum, funktion) => {
+      console.log("accum: ", accum);
+      console.log("funktion: ", funktion);
+      var dummy;
+      eval(`dummy = ${funktion}`)
+      return dummy(accum);
+    }, args[0])
+  },
+  get: (obj, prop) => {
+    console.log("The functions look like: ", obj().functions, " and the prop is ", prop);
+    return new Proxy(() => ({ functions: [...obj().functions, prop] }),
+                     oHandler);
+  },
 };
 
-exports.o = new Proxy(() => ({ not: 'sure' }), oHandler)
+exports.o = new Proxy(function() { return { functions: [] } }, oHandler)
--- spec/for.spec.js
+++ spec/for.spec.js
@@ -1,13 +1,24 @@
-var dig = require('dig-for');
+var dig = require('../index');
 
-it("implements .for", function() {
-  expect(dig.for.one({ one: 1, two: 2, three: 3 })).toEq(1);
-  expect(dig.for.two({ one: 1, two: 2, three: 3 })).toEq(2);
-  expect(dig.for.three({ one: 1, two: 2, three: 3 })).toEq(3);
-  expect(dig.for.one({ one: { two: 2, three: 3 } })).toEq({ two: 2, three: 3 });
-  expect(dig.for.one.two({ one: { two: 2, three: 3 } })).toEq(2);
-  expect(dig.for.one.three({ one: { two: 2, three: 3 } })).toEq(3);
-  expect(dig.for.one({ one: { two: 2 }, three: 3 })).toEq({ two: 2 });
-  expect(dig.for.one.two({ one: { two: 2 }, three: 3 })).toEq(2);
-  expect(dig.for.three({ one: { two: 2 }, three: 3 })).toEq(3);
+describe(".for", () => {
+  it('digs like a mofo', () => {
+    expect(dig.for.one({ one: 1, two: 2, three: 3 })).toEqual(1);
+    expect(dig.for.two({ one: 1, two: 2, three: 3 })).toEqual(2);
+    expect(dig.for.three({ one: 1, two: 2, three: 3 })).toEqual(3);
+    expect(dig.for.one({ one: { two: 2, three: 3 } })).toEqual({ two: 2, three: 3 });
+    expect(dig.for.one.two({ one: { two: 2, three: 3 } })).toEqual(2);
+    expect(dig.for.one.three({ one: { two: 2, three: 3 } })).toEqual(3);
+    expect(dig.for.one({ one: { two: 2 }, three: 3 })).toEqual({ two: 2 });
+    expect(dig.for.one.two({ one: { two: 2 }, three: 3 })).toEqual(2);
+    expect(dig.for.three({ one: { two: 2 }, three: 3 })).toEqual(3);
+  });
+});
+
+describe(".o", () => {
+  it('combines functions', () => {
+    var a = (arg) => 3 * arg;
+    var b = (arg) => 10 + arg;
+    var c = (arg) => arg * arg;
+    expect(dig.o.a.b.c(2)).toEqual(256);
+  });
 });
